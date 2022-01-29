2cdf8e51877b8af6439aab7c3d72e5cd913af716
NatsuX <ketutdiar6@gmail.com>
2022-01-29 13:36:14 +0800

    Fix some a little error

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -19,17 +19,33 @@
 
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/01786058e6.js" crossorigin="anonymous"></script>
+    <!-- Move Some Packages to the top to make sure
+        this package is loaded first before the page is rendered -->
+    <!-- Swiper JS -->
+        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+        <!-- Boostrap Javascript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+        <!-- Jquery -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <!-- Alert -->
+        <script src="/assets/js/sweetalert2.all.min.js"></script>
+
+            <!-- Ionicon -->
+            <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+            <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
     <title>Buku Explore | {{ $title }}</title>
     <link rel="icon" type="image/x-icon" href="/assets/image/icon-logo/book-stack.png">
-    
+
 
     <style>
       body {
         overflow-x: hidden;
         background-color: white;
       }
-      
+
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -54,36 +70,16 @@
     </div>
 
     <!-- End Loader -->
-    
+
     @include('main.part.navbar')
 
-        @yield('main')  
+        @yield('main')
 
     @include('main.part.footer')
 
-
-    <!-- Swiper JS -->
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    
     <!-- Slider JS -->
-    <script src="/assets/js/lightslider.js"></script>
-
-    <!-- Boostrap Javascript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Jquery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
+        <script src="/assets/js/lightslider.js"></script>
     <!-- My Javascript -->
     <script src="/assets/js/script.js"></script>
-
-    <script src="/assets/js/sweetalert2.all.min.js"></script>
-
-
-    <!-- Ionicon -->
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
-
   </body>
-  </html>    
+  </html>
